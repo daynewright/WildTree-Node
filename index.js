7fc6e9ed2e173329fd26f548d4ebf73ff959376e
Dayne Wright <daynewr@gmail.com>
2017-03-08 15:59:43 -0600

    added logic to send emails on different events

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 // include configuration and dependencies
 const nodemailer = require('nodemailer');
 const firebase = require('firebase');
+const moment = require('moment');
 const config = require('./config.js');
 const sender = require('./sender.js');
 
@@ -14,7 +15,7 @@ const fbWorkshops = firebase.database().ref('/workshops');
 const fbUsers = firebase.database().ref('/users');
 
 // CHECKS CHANGES ON ORDERS
-fbOrders.on('child_changed', function(snapshot) {
+fbOrders.on('child_changed', (snapshot) => {
   let userData = snapshot.val();
   let workshopName = '';
   const users = {};
@@ -28,21 +29,7 @@ fbOrders.on('child_changed', function(snapshot) {
       });
     })
     .then((userId) => {
-      fbUsers.once('value')
-      .then((snapshot) => {
-        return new Promise((resolve, reject) => {
-          let data = [];
-          snapshot.forEach((child) => {
-            if(child.val().userId === userId) {
-              users.rep = child.val();
-            }
-            if(child.val().isButcher) {
-              users.butcher = child.val();
-            }
-          });
-          resolve(users);
-        });
-      })
+      getUsers(userId)
       .then((users) => {
         let data = {
           workshop: workshopName,
@@ -51,25 +38,73 @@ fbOrders.on('child_changed', function(snapshot) {
           isSpecialOrder: userData.specialOrder
         };
 
-        console.log('USERS: ', users);
-        console.log('MESSAGE DATA: ', data);
-        sendEmail(users, data);
+        let emailObj = {
+          subject : `The workshop "${data.workshop}" was updated!`,
+          message: `${users.rep.name} (${users.rep.email}) just updated the workshop "${workshopName}"!\nThe bundle ${data.bundleName} is now at a quantity of ${data.newQuantity}.\n`,
+          sendTo: users.butcher.email
+        };
+        emailObj.message += data.isSpecialOrder ? '\n**It is a special order so some meats have changed!**' : '\nThis is a NORMAL order.';
+
+        sendEmail(emailObj);
       });
     });
 
 });
 
-// CHECK FOR NEW OR UPDATED WORKSHOPS
-fbWorkshops.on('child_changed', function(shapshot) {
-  let workshop = snapshot.val();
 
+// CHECK FOR NEW SUBMITTED WORKSHOPS
+fbWorkshops.on('child_changed', (snapshot) => {
+    let workshop = snapshot.val();
 
+    if(workshop.isSubmitted && !workshop.isApproved) {
+      getUsers(workshop.uid)
+      .then((users) => {
+        let emailObj = {
+          subject: `New Workshop "${workshop.name}" submitted for pickup ${moment(workshop.date).format('MM/DD')} @ ${moment(workshop.time).format('hh:mma')}`,
+          message: `A new workshop was just submitted by ${users.rep.name} (${users.rep.email}) for you to review and approve.\n\nWORKSHOP NAME: ${workshop.name}\nWORKSHOP PICKUP: ${moment(workshop.date).format('MM/DD')} @ ${moment(workshop.time).format('hh:mma')}\nREP NAME: ${users.rep.name}\nREP EMAIL: ${users.rep.email}`,
+          sendTo: users.butcher.email
+        }
+        sendEmail(emailObj);
+      });
+    }
 
+    if(workshop.isApproved){
+      getUsers(workshop.uid)
+      .then((users) => {
+        let emailObj = {
+          subject: `Workshop "${workshop.name}" was approved!`,
+          message: `Your workshop "${workshop.name}" was approved for pickup on ${moment(workshop.date).format('MM/DD')} @ ${moment(workshop.time).format('hh:mma')} by ${users.butcher.name} (${users.butcher.email}).\nYou can continue to add orders until 2 days prior.`,
+          sendTo: users.rep.email
+        }
+        sendEmail(emailObj);
+      });
+    }
 });
 
+//Get users helper function
+let getUsers = (userId) => {
+  return new Promise((resolve)=> {
+    let users = fbUsers.once('value')
+      .then((snapshot) => {
+        return new Promise((resolve, reject) => {
+          let data = [];
+          snapshot.forEach((child) => {
+            if(child.val().userId === userId) {
+              data.rep = child.val();
+            }
+            if(child.val().isButcher) {
+              data.butcher = child.val();
+            }
+          });
+          resolve(data);
+        });
+      });
+      resolve(users);
+    });
+};
 
 // Called by watchers to send email
-var sendEmail = function(users, data) {
+var sendEmail = function(emailObj) {
   let smtpTransport = nodemailer.createTransport({
       service: 'gmail',
       auth: {
@@ -78,21 +113,18 @@ var sendEmail = function(users, data) {
       }
   });
 
-  let message = `${users.rep.name} (${users.rep.email}) just updated the workshop ${data.workshop}! \n The bundle ${data.bundleName} is now at a quantity of ${data.newQuantity}.\n`;
-      message += data.isSpecialOrder ? '**It is a special order so some meats have changed!**' : 'This is a NORMAL order.';
-
   let mailOptions = {
     from: "WildTreeApp <wildtreeApp@gmail.com>",
-    to: users.butcher.email,
-    subject: `The workshop "${data.workshop}" was updated!`,
-    text: message
+    to: emailObj.sendTo,
+    subject: emailObj.subject,
+    text: emailObj.message
   };
 
   smtpTransport.sendMail(mailOptions, function(error, response){
     if(error){
       console.log(error);
     } else {
-      console.log("Message sent: " + response.message);
+      console.log("Message sent: " + response.response);
     }
     smtpTransport.close();
   });
