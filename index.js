6fef27f552fb3cfc84ff52f69806a9ef99b935b5
Dayne Wright <daynewr@gmail.com>
2017-03-07 22:35:53 -0600

    first test for node server email

---
+++ index.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const nodemailer = require('nodemailer');
+const firebase = require('firebase');
+var config = require('./config.js')
+
+firebase.initializeApp(config);
+
+const fbOrders = firebase.database().ref('/orders');
+const fbWorkshops = firebase.database().ref('/workshops');
+
+fbOrders.on('child_added', function(snapshot) {
+  let userData = snapshot.val();
+  let smtpTransport = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: "wildtreeapp@gmail.com",
+        pass: "wildtree4L"
+      }
+  });
+
+  let mailOptions = {
+    from: "WildTreeApp <wildtreeApp@gmail.com>",
+    to: 'daynewr@gmail.com',
+    subject: 'Test Email',
+    text: `Just a simple test email to see if something is sent.`
+  };
+
+  smtpTransport.sendMail(mailOptions, function(error, response){
+    if(error){
+      console.log(error);
+    } else {
+      console.log("Message sent: " + response.message);
+    }
+    smtpTransport.close();
+  });
+
+});
